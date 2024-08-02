9cda758e6cc9effe801c0c1270fa50503c2c4318
William Tomé <william.weirich@helioprint.com.br>
2024-08-02 13:48:53 -0300

    feat: Criado serviço para tratamento de imagens

---
+++ app/Services/ImageService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\UploadedFile;
+
+class ImageService
+{
+    public function upload(?UploadedFile $image): ?string
+    {
+        $imageUrl = null;
+        $imageName = time().'.'.$image?->extension();
+
+        if (!is_null($image) && $image->isValid()) {
+            $imageUrl = $image->storeAs('customers', $imageName);
+        }
+
+        return $imageUrl;
+    }
+}
